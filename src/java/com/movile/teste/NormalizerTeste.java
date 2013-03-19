634a07bdb7abe2c4aa325798313365f43a04d2af
brabo <brabo@brabo-pc>
2013-03-19 16:06:30 -0300

    a

--- src/java/com/movile/teste/NormalizerTeste.java
+++ src/java/com/movile/teste/NormalizerTeste.java
@@ -7,6 +7,7 @@ public class NormalizerTeste {
     public static String value = "À Á Â Ã Ä Å Æ Ç È É Ê Ë Ì Í Î Ï Ð Ñ Ò Ó Ô Õ Ö Ø Ù Ú Û Ü Ý Þ ß à á â ã ä å æ ç è é ê ë ì í î ï ð ñ ò ó ô õ ö ø ù ú û ü ý þ ÿ ";
 
     public static void main(String args[]) throws Exception {
+    	//Teste
         System.out.println("Original: " + value);
 
         System.out.println("ASCII   : " + Normalizer.normalize(value, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", ""));
